d8b0cf3fef19a44a345174aa81712cb567d9a5b2
Griesser, Nadege <Nadege.Griesser@vector.com>
2023-01-10 15:10:53 +0100

    add environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  facebookClientId: '1311535669646253',
+  backend: 'freundewerbenfreunde.deta.dev',
+  frontend: 'freundewerbenfreunde.github.io',
+  googleTagManagerId: 'GTM-MGGQZMV'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  facebookClientId: '485957230302704',
+  backend: 'freundewerbenfreunde-local.deta.dev',
+  frontend: 'localhost',
+  googleTagManagerId: 'GTM-MGGQZMV'
+};
