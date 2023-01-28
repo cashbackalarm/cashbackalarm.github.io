155d5837dd4d228e46edf59b11cdb64ab7cd3101
Griesser, Nadege <Nadege.Griesser@vector.com>
2023-01-28 21:40:07 +0100

    remove dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -130,7 +129,6 @@ registerLocaleData(localeDe);
     MatCardModule,
     MatCheckboxModule,
     MatDatepickerModule,
-    MatDialogModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
